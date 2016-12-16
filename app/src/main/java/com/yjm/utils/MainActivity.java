226a48f13cde2f5ea575b690ce5eecfb79e12017
yanjingming <727787745@qq.com>
2016-12-16 19:26:51 +0800

    Initial commit

---
+++ app/src/main/java/com/yjm/utils/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yjm.utils;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
